846ef85b7b5937da1a9a8b2e5e7cd680561f4d6a
THARAGESHWARAN-SATHYAMOORTHY <s.tharageshwaran6343000@gmail.com>
2023-05-20 12:43:57 +0530

    new change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,235 +1,160 @@
 import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-function App() {
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      <p>Welcome to the Food Sharing Platform!</p>
+    </div>
+  );
+}
+
+function Users() {
   const [users, setUsers] = useState([]);
-  const [foodItems, setFoodItems] = useState([]);
-  const [donations, setDonations] = useState([]);
-  const [newUserName, setNewUserName] = useState('');
-  const [newUserEmail, setNewUserEmail] = useState('');
-  const [newFoodItemName, setNewFoodItemName] = useState('');
-  const [newFoodItemDescription, setNewFoodItemDescription] = useState('');
-  const [selectedUserId, setSelectedUserId] = useState('');
-  const [selectedFoodItemId, setSelectedFoodItemId] = useState('');
-  const [newDonationQuantity, setNewDonationQuantity] = useState('');
-  const [newDonationPickupAddress, setNewDonationPickupAddress] = useState('');
-  const [newDonationDeliveryAddress, setNewDonationDeliveryAddress] = useState('');
 
   useEffect(() => {
     fetch('/api/users/')
       .then((response) => response.json())
       .then((data) => setUsers(data));
+  }, []);
 
+  return (
+    <div>
+      <h2>Users</h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} - {user.email}
+          </li>
+        ))}
+      </ul>
+      <Link to="/users/create">Create User</Link>
+    </div>
+  );
+}
+
+function CreateUser() {
+  // Implement logic to create a user
+  return (
+    <div>
+      <h2>Create User</h2>
+      <p>Form to create a new user goes here</p>
+    </div>
+  );
+}
+
+function FoodItems() {
+  const [foodItems, setFoodItems] = useState([]);
+
+  useEffect(() => {
     fetch('/api/fooditems/')
       .then((response) => response.json())
       .then((data) => setFoodItems(data));
+  }, []);
 
+  return (
+    <div>
+      <h2>Food Items</h2>
+      <ul>
+        {foodItems.map((foodItem) => (
+          <li key={foodItem.id}>
+            {foodItem.name} - {foodItem.description}
+          </li>
+        ))}
+      </ul>
+      <Link to="/fooditems/create">Create Food Item</Link>
+    </div>
+  );
+}
+
+function CreateFoodItem() {
+  // Implement logic to create a food item
+  return (
+    <div>
+      <h2>Create Food Item</h2>
+      <p>Form to create a new food item goes here</p>
+    </div>
+  );
+}
+
+function Donations() {
+  const [donations, setDonations] = useState([]);
+
+  useEffect(() => {
     fetch('/api/donations/')
       .then((response) => response.json())
       .then((data) => setDonations(data));
   }, []);
 
-  const createUser = () => {
-    fetch('/api/users/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        name: newUserName,
-        email: newUserEmail,
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        setUsers([...users, data]);
-        setNewUserName('');
-        setNewUserEmail('');
-      });
-  };
-
-  const createFoodItem = () => {
-    fetch('/api/fooditems/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        name: newFoodItemName,
-        description: newFoodItemDescription,
-        nutritional_info: '', // Add any additional fields here
-        allergy_alerts: '', // Add any additional fields here
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        setFoodItems([...foodItems, data]);
-        setNewFoodItemName('');
-        setNewFoodItemDescription('');
-      });
-  };
-
-  const createDonation = () => {
-    fetch('/api/donations/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        donor: selectedUserId,
-        food_item: selectedFoodItemId,
-        quantity: newDonationQuantity,
-        pickup_address: newDonationPickupAddress,
-        delivery_address: newDonationDeliveryAddress,
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        setDonations([...donations, data]);
-        setSelectedUserId('');
-        setSelectedFoodItemId('');
-        setNewDonationQuantity('');
-        setNewDonationPickupAddress('');
-        setNewDonationDeliveryAddress('');
-      });
-  };
-
   const completeDonation = (donationId) => {
-    fetch(`/api/donations/${donationId}/`, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        is_completed: true,
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        const updatedDonations = donations.map((donation) =>
-          donation.id === data.id ? data : donation
-        );
-        setDonations(updatedDonations);
-      });
+    // Implement logic to mark a donation as completed
   };
 
   const deleteDonation = (donationId) => {
-    fetch(`/api/donations/${donationId}/`, {
-      method: 'DELETE',
-    }).then(() => {
-      const updatedDonations = donations.filter((donation) => donation.id !== donationId);
-      setDonations(updatedDonations);
-    });
+    // Implement logic to delete a donation
   };
 
   return (
     <div>
-      <h1 className="header">Food Sharing Platform</h1>
-      <div className="section">
-        <h2>Users</h2>
-        <ul>
-          {users.map((user) => (
-            <li key={user.id}>
-              {user.name} - {user.email}
-            </li>
-          ))}
-        </ul>
-        <div>
-          <input
-            type="text"
-            placeholder="Name"
-            value={newUserName}
-            onChange={(e) => setNewUserName(e.target.value)}
-          />
-          <input
-            type="email"
-            placeholder="Email"
-            value={newUserEmail}
-            onChange={(e) => setNewUserEmail(e.target.value)}
-          />
-          <button onClick={createUser}>Create User</button>
-        </div>
-      </div>
-      <div className="section">
-        <h2>Food Items</h2>
-        <ul>
-          {foodItems.map((foodItem) => (
-            <li key={foodItem.id}>
-              {foodItem.name} - {foodItem.description}
-            </li>
-          ))}
-        </ul>
-        <div>
-          <input
-            type="text"
-            placeholder="Name"
-            value={newFoodItemName}
-            onChange={(e) => setNewFoodItemName(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Description"
-            value={newFoodItemDescription}
-            onChange={(e) => setNewFoodItemDescription(e.target.value)}
-          />
-          <button onClick={createFoodItem}>Create Food Item</button>
-        </div>
-      </div>
-      <div className="section">
-        <h2>Donations</h2>
-        <ul>
-          {donations.map((donation) => (
-            <li key={donation.id}>
-              {donation.donor.name} donated {donation.quantity} {donation.food_item.name}
-              {!donation.is_completed && (
-                <button onClick={() => completeDonation(donation.id)}>Complete</button>
-              )}
-              <button onClick={() => deleteDonation(donation.id)}>Delete</button>
-            </li>
-          ))}
-        </ul>
-        <div>
-          <h3>Create Donation</h3>
-          <select value={selectedUserId} onChange={(e) => setSelectedUserId(e.target.value)}>
-            <option value="">Select User</option>
-            {users.map((user) => (
-              <option key={user.id} value={user.id}>
-                {user.name}
-              </option>
-            ))}
-          </select>
-          <select
-            value={selectedFoodItemId}
-            onChange={(e) => setSelectedFoodItemId(e.target.value)}
-          >
-            <option value="">Select Food Item</option>
-            {foodItems.map((foodItem) => (
-              <option key={foodItem.id} value={foodItem.id}>
-                {foodItem.name}
-              </option>
-            ))}
-          </select>
-          <input
-            type="number"
-            placeholder="Quantity"
-            value={newDonationQuantity}
-            onChange={(e) => setNewDonationQuantity(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Pickup Address"
-            value={newDonationPickupAddress}
-            onChange={(e) => setNewDonationPickupAddress(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Delivery Address"
-            value={newDonationDeliveryAddress}
-            onChange={(e) => setNewDonationDeliveryAddress(e.target.value)}
-          />
-          <button onClick={createDonation}>Create Donation</button>
-        </div>
-      </div>
+      <h2>Donations</h2>
+      <ul>
+        {donations.map((donation) => (
+          <li key={donation.id}>
+            {donation.donor.name} donated {donation.quantity} {donation.food_item.name}
+            {!donation.is_completed && (
+              <button onClick={() => completeDonation(donation.id)}>Complete</button>
+            )}
+            <button onClick={() => deleteDonation(donation.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      <Link to="/donations/create">Create Donation</Link>
     </div>
+  );
+}
+
+function CreateDonation() {
+  // Implement logic to create a donation
+  return (
+    <div>
+      <h2>Create Donation</h2>
+      <p>Form to create a new donation goes here</p>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <h1 className="header">Food Sharing Platform</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+            <li>
+              <Link to="/fooditems">Food Items</Link>
+            </li>
+            <li>
+              <Link to="/donations">Donations</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={Home} />
+        <Route path="/users" exact component={Users} />
+        <Route path="/users/create" component={CreateUser} />
+        <Route path="/fooditems" exact component={FoodItems} />
+        <Route path="/fooditems/create" component={CreateFoodItem} />
+        <Route path="/donations" exact component={Donations} />
+        <Route path="/donations/create" component={CreateDonation} />
+      </div>
+    </Router>
   );
 }
 
