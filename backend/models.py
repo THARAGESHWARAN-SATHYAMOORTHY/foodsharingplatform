bac81d154fbc3a149e01a4743d8ad08b75361e8a
THARAGESHWARAN-SATHYAMOORTHY <s.tharageshwaran6343000@gmail.com>
2023-05-20 07:57:28 +0530

    models update

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(unique=True)
+    # Add additional fields as per your requirements
+
+    def __str__(self):
+        return self.name
+
+class FoodItem(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    # Add additional fields as per your requirements
+
+    def __str__(self):
+        return self.name
+
+class Donation(models.Model):
+    donor = models.ForeignKey(User, on_delete=models.CASCADE)
+    food_item = models.ForeignKey(FoodItem, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    # Add additional fields as per your requirements
+
+    def __str__(self):
+        return f"Donation by {self.donor} - {self.food_item}"
