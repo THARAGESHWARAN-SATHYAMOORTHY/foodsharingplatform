666e77d437d19b73199fc5225fee960e80164b02
THARAGESHWARAN-SATHYAMOORTHY <s.tharageshwaran6343000@gmail.com>
2023-05-20 08:52:55 +0530

    forntent update

--- foodsharingplatform/settings.py
+++ foodsharingplatform/settings.py
@@ -30,16 +30,21 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'corsheaders',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -125,3 +130,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_ALLOW_ALL = True
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [foodItems, setFoodItems] = useState([]);
+  const [donations, setDonations] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/users/')
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+
+    fetch('/api/fooditems/')
+      .then((response) => response.json())
+      .then((data) => setFoodItems(data));
+
+    fetch('/api/donations/')
+      .then((response) => response.json())
+      .then((data) => setDonations(data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="header">Food Sharing Platform</h1>
+      <div className="section">
+        <h2>Users</h2>
+        <ul>
+          {users.map((user, index) => (
+            <li key={user.id}>
+              {index + 1}. {user.name} - {user.email}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="section">
+        <h2>Food Items</h2>
+        <ul>
+          {foodItems.map((foodItem, index) => (
+            <li key={foodItem.id}>
+              {index + 1}. {foodItem.name} - {foodItem.description}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="section">
+        <h2>Donations</h2>
+        <ul>
+          {donations.map((donation, index) => (
+            <li key={donation.id}>
+              {index + 1}. {donation.donor.name} donated {donation.quantity} {donation.food_item.name}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
